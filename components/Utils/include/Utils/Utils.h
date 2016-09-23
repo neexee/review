f500de13af1b36e7b96c8d9e7db8d4b8ba74fff3
neexee <machulskissv@gmail.com>
2016-09-23 20:43:08 +0700

    Add newline

--- components/Utils/include/Utils/Utils.h
+++ components/Utils/include/Utils/Utils.h
@@ -8,6 +8,7 @@ template <typename... Args>
 std::string string_format(const std::string& format, Args... args);
 
 std::vector<std::string> SplitPath(const std::string& path);
+
 } // namespace utils
 
 #include "Utils.inl"
