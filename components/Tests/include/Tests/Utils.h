318116f3a4703f53855a9153f81467eee927b562
neexee <machulskissv@gmail.com>
2016-09-29 21:25:55 +0700

    Init tests and coverage reports

---
+++ components/Tests/include/Tests/Utils.h
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+namespace tests {
+
+class UtilsTest : public ::testing::Test
+{
+protected:
+	UtilsTest();
+
+	virtual ~UtilsTest();
+};
+
+} // namespace tests
---
+++ components/Tests/src/Utils.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <Tests/Utils.h>
+#include <Utils/Utils.h>
+
+namespace tests {
+
+UtilsTest::UtilsTest()
+{
+}
+
+UtilsTest::~UtilsTest()
+{
+}
+
+TEST_F(UtilsTest, SplitPathSimleSplit)
+{
+	std::string input = "some_dir/any_file.cpp";
+	std::vector<std::string> expected_result{"some_dir", "any_file.cpp"};
+	auto result = utils::SplitPath(input);
+	EXPECT_EQ(result, expected_result);
+}
+
+} // namespace tests
